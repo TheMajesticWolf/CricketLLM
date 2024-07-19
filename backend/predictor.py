8fa7f94cdd97782ac4dbc6732ff3596b8bcdbded
TheMajesticWolf <TheMajesticWolf@wolf.com>
2024-07-19 12:52:57 +0530

    Added new embeddings and added question-answering model deberta-v3-large-squad2

--- backend/predictor.py
+++ backend/predictor.py
@@ -8,7 +8,10 @@ from sentence_transformers import SentenceTransformer, util
 
 given_question = json.loads(sys.stdin.read())["question"]
 
-chunks_and_embeddings_df = pd.read_json("/home/aditya/Desktop/Aditya/cricket-llm/venv/combined_embeddings.json")
+chunks_and_embeddings_df = pd.concat([
+	pd.read_json("../Dataset/combined_embeddings.json"),
+	pd.read_json("../Dataset/combined_embeddings_player_specific.json")
+], ignore_index=True)
 
 embeddings = np.array(chunks_and_embeddings_df["embedding"].to_list())
 
@@ -48,6 +51,34 @@ for answer in relavant:
 
 
 
+from transformers import pipeline, AutoTokenizer, AutoModelForQuestionAnswering
+
+
+# model_name = "bert-large-uncased-whole-word-masking-finetuned-squad"
+
+# tokenizer = AutoTokenizer.from_pretrained(model_name)
+# model = AutoModelForQuestionAnswering.from_pretrained(model_name, low_cpu_mem_usage=True)
+
+# device = "cuda" if torch.cuda.is_available() else "cpu"
+# # model.to(device)
+
+# question_answerer = pipeline("question-answering", model=model, tokenizer=tokenizer, device="cuda")
+
+model_name = "deepset/deberta-v3-large-squad2"
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForQuestionAnswering.from_pretrained(model_name, low_cpu_mem_usage=True).to("cuda")
+
+question_answerer = pipeline('question-answering', model=model, tokenizer=tokenizer, device="cuda")
+
+
+
+
+generated_text = question_answerer(question=given_question, context=response)
+# print(generated_text)
+
+
+
 if(response == "empty"):
 
 	response = "Sorry, I am currently not trained on the data relavent to your question"
@@ -57,7 +88,7 @@ if(response == "empty"):
 
 data_to_send = json.dumps({
 
-	"output": response,
+	"output": generated_text["answer"],
 	"question": given_question,
 	"return_format": "string"
 
